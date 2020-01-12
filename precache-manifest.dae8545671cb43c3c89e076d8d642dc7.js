c1a02a97afcf741c73cb7a1244e2df5d55780c86
Ali Haydar <ali.haydar85@gmail.com>
2020-01-12 21:07:31 +1300

    Updates

---
+++ precache-manifest.dae8545671cb43c3c89e076d8d642dc7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4835af9083fbbdd55976c3e254e364c5",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "9c8f9933e0cf92b08986",
+    "url": "/personal-website/static/css/main.dc8b5341.chunk.css"
+  },
+  {
+    "revision": "75c79cae6f4e82498d1d",
+    "url": "/personal-website/static/js/2.7aab5e46.chunk.js"
+  },
+  {
+    "revision": "9c8f9933e0cf92b08986",
+    "url": "/personal-website/static/js/main.f0ae81cb.chunk.js"
+  },
+  {
+    "revision": "f5a60a27ec8e0ba479a2",
+    "url": "/personal-website/static/js/runtime-main.afaf4a9a.js"
+  },
+  {
+    "revision": "90564c2eea3d73e514ece3f593f5ed98",
+    "url": "/personal-website/static/media/BlueGradientBackground.90564c2e.svg"
+  },
+  {
+    "revision": "fcd9c7851385ff709d763bad58f5f49b",
+    "url": "/personal-website/static/media/EmailIcon.fcd9c785.svg"
+  },
+  {
+    "revision": "df7de487437f82c0e60862558e93064f",
+    "url": "/personal-website/static/media/GithubIcon.df7de487.svg"
+  },
+  {
+    "revision": "f9fc260f5ad5e497c7cb3a31afd1a9ed",
+    "url": "/personal-website/static/media/HomePageMainImage.f9fc260f.svg"
+  },
+  {
+    "revision": "76d09e0f4530306f807aff4abdc839db",
+    "url": "/personal-website/static/media/LinkedinIcon.76d09e0f.svg"
+  },
+  {
+    "revision": "1bd48bb6f6f9f72a62e5680ac59e35a1",
+    "url": "/personal-website/static/media/TwitterIcon.1bd48bb6.svg"
+  },
+  {
+    "revision": "ae96b39b410151d5e4a626bb25efe8b7",
+    "url": "/personal-website/static/media/WorkIcon1.ae96b39b.svg"
+  },
+  {
+    "revision": "51623175f6afad73e7539a9614939d20",
+    "url": "/personal-website/static/media/WorkIcon2.51623175.svg"
+  },
+  {
+    "revision": "850c1be39d3c7013e243153aa6b06511",
+    "url": "/personal-website/static/media/WorkIcon3.850c1be3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.07a9f03ef2401b85d2b9ddb1df518c37.js"
+  "/personal-website/precache-manifest.dae8545671cb43c3c89e076d8d642dc7.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/personal-website/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
